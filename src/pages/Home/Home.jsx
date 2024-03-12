a8fd6cb5b6bc24ecad67fc3e4f2adb39ba52d3c7
Timoté LANCELLE <timote.lancelle@outlook.fr>
2024-03-12 14:28:54 +0100

    Mise en place DatePicker

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,22 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import "./Home.scss";
 import Input from "../../components/Input/Input";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function Home() {
+    const [dateOfBirth, setDateOfBirth] = useState(null);
+    const [startDate, setStartDate] = useState(null);
+
+    const handleDateOfBirthChange = (date) => {
+        setDateOfBirth(date);
+    };
+
+    const handleStartDateChange = (date) => {
+        setStartDate(date);
+    };
+
     return (
         <div>
             <Header />
@@ -12,6 +26,14 @@ function Home() {
                 <form>
                     <Input htmlFor={"first_name"} labelText={"First Name"} type={"text"} inputId={"first_name"} />
                     <Input htmlFor={"last_name"} labelText={"Last Name"} type={"text"} inputId={"last_name"} />
+                    <div>
+                        <label htmlFor="date_of_birth">Date of Birth</label>
+                        <DatePicker selected={dateOfBirth} onChange={handleDateOfBirthChange} />
+                    </div>
+                    <div>
+                        <label htmlFor="start_date">Start Date</label>
+                        <DatePicker selected={startDate} onChange={handleStartDateChange} />
+                    </div>
                     <fieldset>
                         <legend>Address</legend>
                         <Input htmlFor={"street"} labelText={"Street"} type={"text"} inputId={"street"} />
