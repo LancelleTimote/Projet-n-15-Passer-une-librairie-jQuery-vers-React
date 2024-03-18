ac3479cfd2c10af58174a0798f9639f31342c451
Timoté LANCELLE <timote.lancelle@outlook.fr>
2024-03-19 00:18:19 +0100

    Modification utilisation material-react-table et plus tanstack-react-table

--- src/pages/EmployeeList/EmployeeList.jsx
+++ src/pages/EmployeeList/EmployeeList.jsx
@@ -1,57 +1,64 @@
+import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
-import "./EmployeeList.scss";
-import { useReactTable, createColumnHelper, flexRender, getCoreRowModel } from "@tanstack/react-table";
+import { MaterialReactTable, useMaterialReactTable } from "material-react-table"; // Importation des composants de Material React Table
 import { useSelector } from "react-redux";
+import "./EmployeeList.scss";
 
 function EmployeeList() {
-    const columnHelper = createColumnHelper();
-
-    const columns = [
-        columnHelper.accessor("firstName", {
-            cell: (info) => info.getValue(),
-            header: () => "First Name",
-        }),
-        columnHelper.accessor("lastName", {
-            cell: (info) => info.getValue(),
-            header: () => "Last Name",
-        }),
-        columnHelper.accessor("dateOfBirth", {
-            cell: (info) => info.getValue(),
-            header: () => "Date of Birth",
-        }),
-        columnHelper.accessor("startDate", {
-            cell: (info) => info.getValue(),
-            header: () => "Start Date",
-        }),
-        columnHelper.accessor("street", {
-            cell: (info) => info.getValue(),
-            header: () => "Street",
-        }),
-        columnHelper.accessor("city", {
-            cell: (info) => info.getValue(),
-            header: () => "City",
-        }),
-        columnHelper.accessor("state", {
-            cell: (info) => info.getValue(),
-            header: () => "State",
-        }),
-        columnHelper.accessor("zipCode", {
-            cell: (info) => info.getValue(),
-            header: () => "Zip Code",
-        }),
-        columnHelper.accessor("department", {
-            cell: (info) => info.getValue(),
-            header: () => "Department",
-        }),
-    ];
-
     const employees = useSelector((state) => state.employee.employees);
 
-    const table = useReactTable({
-        data: employees,
+    const columns = [
+        {
+            accessorKey: "firstName",
+            header: "First Name",
+            size: 150,
+        },
+        {
+            accessorKey: "lastName",
+            header: "Last Name",
+            size: 150,
+        },
+        {
+            accessorKey: "dateOfBirth",
+            header: "Date of Birth",
+            size: 150,
+        },
+        {
+            accessorKey: "startDate",
+            header: "Start Date",
+            size: 150,
+        },
+        {
+            accessorKey: "street",
+            header: "Street",
+            size: 150,
+        },
+        {
+            accessorKey: "city",
+            header: "City",
+            size: 150,
+        },
+        {
+            accessorKey: "state",
+            header: "State",
+            size: 150,
+        },
+        {
+            accessorKey: "zipCode",
+            header: "Zip Code",
+            size: 150,
+        },
+        {
+            accessorKey: "department",
+            header: "Department",
+            size: 150,
+        },
+    ];
+
+    const table = useMaterialReactTable({
         columns,
-        getCoreRowModel: getCoreRowModel(),
+        data: employees,
     });
 
     return (
@@ -60,26 +67,7 @@ function EmployeeList() {
             <div className="flexPlacement gradientColor employeeList_container">
                 <div className="employeeList_container_content">
                     <h1 className="employeeList_container_content_title">Current Employees</h1>
-                    <table>
-                        <thead>
-                            {table.getHeaderGroups().map((headerGroup) => (
-                                <tr key={headerGroup.id}>
-                                    {headerGroup.headers.map((header) => (
-                                        <th key={header.id}>{header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}</th>
-                                    ))}
-                                </tr>
-                            ))}
-                        </thead>
-                        <tbody>
-                            {table.getRowModel().rows.map((row) => (
-                                <tr key={row.id}>
-                                    {row.getVisibleCells().map((cell) => (
-                                        <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
-                                    ))}
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                    <MaterialReactTable table={table} />
                 </div>
             </div>
             <Footer />
